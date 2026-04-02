b411648478b5181719ddd89e81d49e66507267ed
katerinacookies <sokolyt@gmail.com>
2026-04-02 16:25:07 +0300

    Build files, themes, colors and main activity

--- app/src/main/java/com/example/prikazki/MainActivity.java
+++ app/src/main/java/com/example/prikazki/MainActivity.java
@@ -1,8 +1,25 @@
 package com.example.prikazki;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.AnimateBuilder;
+import com.aldebaran.qi.sdk.builder.AnimationBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 

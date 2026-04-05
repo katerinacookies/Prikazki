341f05f75ad1f7a3a3a39cb46b7cd26024de6012
katerinacookies <sokolyt@gmail.com>
2026-04-05 14:55:14 +0300

    Audio converts runAnimation method

--- app/src/main/java/com/example/prikazki/RobotHelper.java
+++ app/src/main/java/com/example/prikazki/RobotHelper.java
@@ -2,7 +2,29 @@ package com.example.prikazki;
 
 //metodite za audio, animacii i tn
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.AnimateBuilder;
+import com.aldebaran.qi.sdk.builder.AnimationBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RobotHelper {
+    private QiContext qiContext;
     private void loadTaleData()
     {
 
@@ -16,6 +38,25 @@ public class RobotHelper {
     private void playAnimationChain()
     {
 
+    }
+
+    private void runAnimation(int resId, Runnable onComplete) {
+        AnimationBuilder.with(qiContext)
+                .withResources(resId)
+                .buildAsync()
+                .andThenConsume(animation -> {
+                    AnimateBuilder.with(qiContext)
+                            .withAnimation(animation)
+                            .buildAsync()
+                            .andThenConsume(animate -> {
+                                // Run the animation and wait for it to finish
+                                animate.async().run().andThenConsume(ignored -> {
+                                    if (onComplete != null) {
+                                        onComplete.run();
+                                    }
+                                });
+                            });
+                });
     }
 
 }
